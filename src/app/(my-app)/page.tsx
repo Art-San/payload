6bf6157120c0764ce9009c8cc0f13b7431a8e346
Art San <artsan.mcc@gmail.com>
2024-12-09 00:26:46 +0700

    ok

---
+++ src/app/(my-app)/page.tsx
@@ -0,0 +1,10 @@
+// import { MoviesListServer } from '@/modules/movies-list'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>dddd</h1>
+      {/* <MoviesListServer /> */}
+    </main>
+  )
+}
